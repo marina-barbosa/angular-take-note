56ff372c83af258b5d6d2b133701e7d437cc6e3f
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-04-26 23:26:45 -0300

    add service export

--- src/app/services/export.service.ts
+++ src/app/services/export.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,38 @@ import { Injectable } from '@angular/core';
 })
 export class ExportService {
 
-  constructor() { }
+  url = 'http://localhost:5036/v1/export/';
+
+  constructor(private http: HttpClient) { }
+
+  exportTxt(): void {
+    this.http.get(this.url + 'txt', { responseType: 'text' })
+      .subscribe((data: string) => {
+        this.downloadFile(data, 'text/plain', 'registro.txt');
+      });
+  }
+
+  exportCsv(): void {
+    this.http.get(this.url + 'csv', { responseType: 'text' })
+      .subscribe((data: string) => {
+        this.downloadFile(data, 'text/csv', 'registro.csv');
+      });
+  }
+
+  exportXls(): void {
+    this.http.get(this.url + 'xls', { responseType: 'arraybuffer' })
+      .subscribe((data: ArrayBuffer) => {
+        this.downloadFile(data, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'LogEntrys.xlsx');
+      });
+  }
+
+  private downloadFile(data: any, type: string, filename: string): void {
+    const blob = new Blob([data], { type: type });
+    const url = window.URL.createObjectURL(blob);
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.download = filename;
+    anchor.click();
+    window.URL.revokeObjectURL(url);
+  }
 }
