0c08ff011f7c248a895ca54a33f9dfe7ce684bf7
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-04-27 19:50:05 -0300

    add service getall

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
 
-  constructor() { }
+  url = 'https://takenotemxm.azurewebsites.net/v1/note/';
+
+  constructor(private http: HttpClient) { }
+
+  getAllNotes(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      catchError((error: any) => {
+        console.error('An error occurred:', error);
+        return throwError(() => new Error('Algo deu errado.'));
+      })
+    );
+  }
 }
